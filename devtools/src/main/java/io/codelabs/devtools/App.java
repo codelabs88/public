d9806e2021291ca225e0a4b218c6a64c61099245
quymv88 <quymv88@gmail.com>
2022-07-28 23:35:57 +0700

    init

---
+++ devtools/src/main/java/io/codelabs/devtools/App.java
@@ -0,0 +1,41 @@
+package io.codelabs.devtools;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.stream.IntStream;
+
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(MainVerticle.class.getName());
+        WebClientOptions options = new WebClientOptions()
+            .setMaxPoolSize(10000)
+            .setKeepAlive(true)
+            .setSsl(false);
+
+        Vertx vertx1 = Vertx.vertx();
+        WebClient client = WebClient.create(vertx1, options);
+
+        IntStream.range(0, 30000)
+            .parallel()
+            .forEach(i -> {
+                long start = System.currentTimeMillis();
+                client.get(8080, "localhost", "/")
+                    .send()
+                    .onSuccess(response -> {
+                        logger.info("OK: {} ms", System.currentTimeMillis() - start);
+                    })
+                    .onFailure(throwable -> {
+                        throwable.printStackTrace();
+                    });
+            });
+    }
+}
